cf95a7d3f0b1cd225a3ffae51c16c0c6b0522e3f
Mario <31197372+Max131@users.noreply.github.com>
2025-11-18 13:51:14 -0600

    docs(Chirp.php): añadir documentación para la relación user en el modelo Chirp

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -11,7 +11,11 @@ class Chirp extends Model
         'message',
     ];
 
-
+    /**
+     * Get the user that owns the chirp.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
