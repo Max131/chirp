24c0d5fec8044a2c19f3751b6dbd8be9a22f9c4f
Mario <31197372+Max131@users.noreply.github.com>
2025-12-02 11:59:21 -0600

    fix(layout.blade.php): actualizar el enlace de "Sign In" a la ruta /login

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,7 +18,7 @@
             <a href="/" class="btn btn-ghost text-xl">🐦 Chirper</a>
         </div>
         <div class="navbar-end gap-2">
-            <a href="#" class="btn btn-ghost btn-sm">Sign In</a>
+            <a href="/login" class="btn btn-ghost btn-sm">Sign In</a>
             <a href="/register" class="btn btn-primary btn-sm">Sign Up</a>
         </div>
     </nav>
