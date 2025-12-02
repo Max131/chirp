ec13ec694d72627fe5e029656f0a8c7c6d563d94
Mario <31197372+Max131@users.noreply.github.com>
2025-12-02 11:05:30 -0600

    fix(ChirpController.php): cambiar el parámetro de la función edit a Chirp feat(ChirpController.php): devolver la vista de edición con el chirp correspondiente

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -62,9 +62,10 @@ class ChirpController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Chirp $chirp)
     {
-        //
+
+        return view('chirps.edit', compact('chirp'));
     }
 
     /**
